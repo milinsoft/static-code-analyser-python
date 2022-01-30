9d5f90d2817f53becd608ab015c3486b22762ead
parseltongist <>
2022-01-30 16:52:05 +0100

    Stage 1/5 initial commit

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,1 +1,15 @@
-# write your code here
+from os.path import exists
+
+
+path_to_file = input()
+
+if not exists(path_to_file):
+    exit()
+
+with open(path_to_file, "r") as file:
+    code = [x.strip("\n") for x in file.readlines()]
+
+
+for line in range(len(code)):
+    if len(code[line]) > 49:
+        print(f"Line {line + 1}: S001 Too long. Please make sure that each line is no longer than 49 characters")
