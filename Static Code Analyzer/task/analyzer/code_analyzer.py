85e3a20918796e68d6793128a17754046a3ff696
parseltongist <>
2022-02-13 17:34:00 +0100

    Stage 3/5 draft

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,4 +1,7 @@
+import re
+import sys
 from os.path import exists
+import os
 
 errors = {
     "S001": "Too long. Please make sure that each "
@@ -10,45 +13,97 @@ errors = {
     "S004": "Less than two spaces before inline comments",
     "S005": "TODO found (in comments only and case-insensitive)",
     "S006": "More than two blank lines preceding a code "
-            "line (applies to the first non-empty line)."
+            "line (applies to the first non-empty line).",
+    "S007": "Too many spaces after construction_name (def or class)",
+    "S008": "Class name class_name should be written in CamelCase",
+    "S009": "Function name function_name should be written in snake_case.",
 }
 
 
-path_to_file = input()
+def scan_code(code, _path):
+    # Iterating over the lines
+    # mode
 
-if not exists(path_to_file):
-    exit()
+    for line in range(len(code)):
+        comment_start_index = code[line].find("#")
 
-with open(path_to_file, "r") as file:
-    code: list = [x.strip("\n") for x in file.readlines()]
+        if len(code[line]) > 49:
+            print(f"{_path}: Line {line + 1}: S001", errors["S001"])
+
+        if len(code[line]) >= 5:
+            identation = len(code[line]) - len(code[line].lstrip(" "))
+            if identation % 4 != 0:
+                print(f"{_path}: Line {line + 1}: S002", errors["S002"])
+
+        if ";" in code[line]:
+            if any([comment_start_index == -1 and code[line][-1] == ";",  # semicolon as a last symbol in the string (to avoid fake warning)
+                    comment_start_index > code[line].find(";")]):
+                print(f"{_path}: Line {line + 1}: S003", errors["S003"])
+
+        if len(code[line]) >= 3 and "#" in code[line]:
+            if comment_start_index > 0:
+                if code[line][comment_start_index-2:].find("  ") != 0:
+                    print(f"{_path}: Line {line + 1}: S004", errors["S004"])
+
+        if "todo" in code[line].lower():
+            if comment_start_index != -1 and comment_start_index < code[line].lower().find("todo"):
+                print(f"{_path}: Line {line + 1}: S005", errors["S005"])
+
+        if line >= 2 and code[line] != "":
+            if code[line-1] == code[line-2] == code[line-3]:
+                print(f"{_path}: Line {line + 1}: S006", errors["S006"])
+
+        """
+        if code[line].startswith("class"):
+            if code[line][5:8] == "  ":
+                print(f"{_path}: Line {line + 1}: S007", errors["S007"])
+            if "_" in line[8::]:
+                print(f"{_path}: Line {line + 1}: S008", errors["S008"])
+
+        if code[line].startswith("def"):
+            if code[line][3:6] == "  ":
+                print(f"{_path}: Line {line + 1}: S007", errors["S007"])
+
+            if not re.match("_*[a-z]+_[a-z]+_{0,2}", code[line][3:6]):
+                print(f"{_path}: Line {line + 1}: S009", errors["S009"])
+        """
+
+# "/Users/aleksander/PycharmProjects/Static Code Analyzer/Static Code Analyzer/task/test"
+# "/Users/aleksander/Desktop/Python"
+
+if len(sys.argv) != 2:
+    exit(print("error. 2 positional arguments expected."))
 
 
-# Iterating over the lines
-for line in range(len(code)):
-    comment_start_index = code[line].find("#")
+path = sys.argv[1].lower()
 
-    if len(code[line]) > 49:
-        print(f"Line {line + 1}: S001", errors["S001"])
-
-    if len(code[line]) >= 5:
-        identation = len(code[line]) - len(code[line].lstrip(" "))
-        if identation % 4 != 0:
-            print(f"Line {line + 1}: S002", errors["S002"])
-
-    if ";" in code[line]:
-        if any([comment_start_index == -1 and code[line][-1] == ";",
-                comment_start_index > code[line].find(";")]):
-            print(f"Line {line + 1}: S003", errors["S003"])
-
-    if len(code[line]) >= 3 and "#" in code[line]:
-        if comment_start_index > 0 and code[line].find("  ") != comment_start_index - 2:
-            print(f"Line {line + 1}: S004", errors["S004"])
+mode = "file" if path.endswith(".py") else "dir"
 
 
-    if "todo" in code[line].lower():
-        if comment_start_index != -1 and comment_start_index < code[line].lower().find("todo"):
-            print(f"Line {line + 1}: S005", errors["S005"])
+if not exists(path):
+   exit(print('path does not exist'))
 
-    if line >= 2 and code[line] != "":
-        if code[line-1] == code[line-2] == code[line-3]:
-            print(f"Line {line + 1}: S006", errors["S006"])
+
+if mode == "file":
+
+    with open(path, "r") as file:
+        code_as_list: list = [x.strip("\n") for x in file.readlines()]
+        scan_code(code_as_list, path)
+
+else:
+
+    current_path = os.getcwd()
+    os.chdir(path)
+
+    python_scripts: list = sorted([x.name for x in os.scandir() if x.name.endswith(".py")])
+
+    _all = sorted([x.name for x in os.scandir()])
+
+
+    for script in python_scripts:
+
+        with open(script, "r") as file:
+            code_as_list: list = [x.strip("\n") for x in file.readlines()]
+            scan_code(code_as_list, script)
+
+
